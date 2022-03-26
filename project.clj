7dd26d3aeded79fa87a2f4ace1bde82a545ec82b
pascal <pascal@dutilsoft.be>
2022-03-26 01:00:12 +0100

    Step 02-Playing in REPL with jdbc

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.github.seancorfield/next.jdbc "1.2.772"]
+                 [com.microsoft.sqlserver/mssql-jdbc "10.2.0.jre11"]]
   :main ^:skip-aot sqlinspector.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
