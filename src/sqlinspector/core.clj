5540dbf20b6852f19a2794f9464fc66d28180166
pascal <pascal@dutilsoft.be>
2022-03-26 01:00:12 +0100

    Step 02-Playing in REPL with jdbc

--- src/sqlinspector/core.clj
+++ src/sqlinspector/core.clj
@@ -1,7 +1,75 @@
 (ns sqlinspector.core
-  (:gen-class))
+  (:gen-class)
+  (:require [next.jdbc :as jdbc])
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+;;----------------------------------------------------------------------------------------
+;; Below is a big chunk of comment. This is used to enter expressions in the REPL directly
+;; from within the Editor.
+;;----------------------------------------------------------------------------------------
+(comment
+
+  ;; the connection parameters
+  (def db {:dbtype "sqlserver"
+           :user "test_user"
+           :password "test123!"
+           :host "127.0.0.1"
+           :encrypt false
+           :dbname "test_db"})
+  (def ds (jdbc/get-datasource db))
+
+  ;; test with the most simple select
+  (jdbc/execute! ds ["select 123 as just_a_number"])
+
+  ;; create some tables
+  (jdbc/execute! ds [(str "create table t_customer ( \n"
+                          "  id_customer int not null identity(1,1) \n"
+                          "    constraint pk_t_customer primary key, \n"
+                          "  first_name varchar(250), \n"
+                          "  last_name varchar(250), \n"
+                          "  last_modified datetime not null \n"
+                          "    constraint df_t_customer default (getdate()))")])
+
+  (jdbc/execute! ds [(str "create table t_address_type ( \n"
+                          "  address_type varchar(50) not null \n"
+                          "    constraint pk_t_address_type primary key, \n"
+                          "  info varchar(250))")])
+
+  (jdbc/execute! ds [(str "create table t_customer_address ( \n"
+                          "  id_customer_address int not null identity(1,1) \n"
+                          "    constraint pk_t_customer_address primary key, \n"
+                          "  id_customer int not null \n"
+                          "    constraint fk_t_customer_address__customer \n"
+                          "    foreign key references t_customer(id_customer), \n"
+                          "  address_type varchar(50) not null \n"
+                          "    constraint fk_t_customer_address__addres_type \n"
+                          "    foreign key references t_address_type(address_type), \n"
+                          "  is_default bit not null \n "
+                          "    constraint df_t_customer_address default (0), \n"
+                          "  info varchar(250))")])
+
+  ;; get table names
+  (jdbc/execute! ds [(str "select name, create_date, modify_date as table_name \n"
+                          "from sys.tables order by name") ])
+
+  ;; get columns of a table
+  (let [table-name "t_customer"]
+    (jdbc/execute! ds
+                   [(str "select \n"
+                         "  c.column_id, \n"
+                         "  c.name as column_name, \n"
+                         "  t.[name] as type_name, \n"
+                         "  c.max_length, \n"
+                         "  c.is_nullable, \n"
+                         "  c.is_identity \n"
+                         "from sys.columns c \n"
+                         "join sys.types t on t.system_type_id = c.system_type_id \n"
+                         "where c.[object_id] = object_id(?) \n"
+                         "order by c.column_id")
+                    table-name]))
+  )
